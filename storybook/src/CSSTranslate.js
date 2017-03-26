8a88c0372226c0ecad48012b43516b3ba8fcf992
Leandro Lemos <llemos@atlassian.com>
2017-03-26 21:00:50 +1100

    Updates

---
+++ storybook/src/CSSTranslate.js
@@ -0,0 +1,8 @@
+module.exports = function (position, axis) {
+    var positionCss = (axis === 'horizontal') ? [position, 0, 0] : [0, position, 0];
+    var transitionProp = 'translate3d';
+
+    var translatedPosition = '(' + positionCss.join(',') + ')';
+
+    return transitionProp + translatedPosition;
+};
